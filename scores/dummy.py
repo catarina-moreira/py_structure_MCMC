33dbefb1d2a0d29a5fd31622a6307ec556e036d4
Catarina Moreira <catarina.p.moreira@ist.utl.pt>
2024-02-01 10:41:30 +1100

    Update dummy.py

--- scores/dummy.py
+++ scores/dummy.py
@@ -1,112 +1,101 @@
 
 from abc import ABC, abstractmethod
-import networkx as nx
 
-import pandas as pd
 import numpy as np
 
-from scipy.special import gammaln
-from scipy.stats import invgamma
-from scipy.stats import multivariate_normal
-
-import statsmodels.api as sm
+import pandas as pd
 
 from scores.ScoreAbstract import Score
 
 
 class DummyScore(Score):
 
-    ##
-    ## Constructor
-    ####################################################################
-    def __init__(self, data : pd.DataFrame, graph : nx.DiGraph = None, isLogSpace = True):
+    def __init__(self, data : pd.DataFrame, incidence : np.ndarray, isLogSpace = False):
         """_summary_
 
         Args:
             data (pd.DataFrame): _description_
             graph (nx.DiGraph, optional): _description_. Defaults to None.
         """
-        super().__init__(data)
-        self.graph = graph
+        super().__init__(data, incidence)
+        
+        self.incidence = incidence
+        self.data = data
         self.isLogSpace = isLogSpace
-        self.am = 1
-        self.aw = len(data.columns) + self.am + 1
-        self.t = self.am * (self.aw - len(data.columns) - 1)/(self.am + 1)
-        self.parameters = {}
-        self.reg_coefficients = {}
-        self.to_string = "BGe Score"
+        
+        self.node_labels = list(data.columns)
     
-    ##
-    ## Compute the marginal likelihood for the data
-    ####################################################################
     def compute(self):
-        """_summary_
+        
+        parameters = {}
+        total_ml = 0
+        score_node = 1
+        
+        # Loop through each node in the graph
+        for node in self.node_labels:
+            
+            parameters[node] = {
+            'score' : score_node
+            }
+            
+            total_ml = total_ml + score_node
 
-        Returns:
-            _type_: _description_
-        """
+        res = {
+            'score': total_ml,
+            'parameters': parameters
+        }
         
-        res = {}
-        res['score'] = 1
-        
-        # number of edges of graph
-        #res['score']= 0#len(self.graph.edges)
-        
-        res['parameters'] = {}
         return res
     
     def compute_node(self, node):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
         
-        res = {}
-        res['score'] = 1
+        parameters = {}
+        score_node = 1
         
-        # number of edges of graph
-        #res['score']= 0#len(self.graph.edges)
+        parameters[node] = {
+            'score' : score_node}
         
-        res['parameters'] = {}
+        res = {
+            'score': score_node,
+            'parameters': parameters
+        }
         return res
     
     
-    # GETTERS AND SETTERS
+    # GETTERS
     #####################################################
 
-    def get_graph(self):
-        return self.graph
+    def to_string():
+        return "Dummy Score"
     
-    def set_graph(self, graph):
-        self.graph = graph
+    def get_to_string(self):
+        return "Dummy Score"
+    
+
+    def get_incidence(self):
+        return self.incidence
+    
+    def get_data(self):
+        return self.data
+    
+    def get_node_labels(self):
+        return self.node_labels
+    
+    def get_isLogSpace(self):
+        return self.isLogSpace
+    
+    # SETTERS
+    #####################################################
+
+    def set_incidence(self, incidence):
+        self.incidence = incidence
         
-    def get_am(self):
-        return self.am
-    
-    def set_am(self, am):
-        self.am = am
+    def set_data(self, data):
+        self.data = data
         
-    def get_aw(self):
-        return self.aw
-    
-    def set_aw(self, aw):
-        self.aw = aw
+    def set_node_labels(self, node_labels):
+        self.node_labels = node_labels
         
-    def get_t(self):
-        return self.t
-    
-    def set_t(self, t):
-        self.t = t
+    def set_isLogSpace(self, isLogSpace):
+        self.isLogSpace = isLogSpace
         
-    def get_parameters(self):
-        return self.parameters
-    
-    def set_parameters(self, parameters):
-        self.parameters = parameters
-        
-    def get_reg_coefficients(self):
-        return self.reg_coefficients
-    
-    def set_reg_coefficients(self, reg_coefficients):
-        self.reg_coefficients = reg_coefficients
